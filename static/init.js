d4f13096ff853a4bf30db1bcdd13087822d4c932
Jonathan Hightower <hightower@fastmail.com>
2019-08-11 13:56:47 -0700

    added drawing functionality

--- static/init.js
+++ static/init.js
@@ -1,1 +1,22 @@
 console.log("This file will initialize the canvas for drawing.")
+
+var canvas = new fabric.Canvas('c');
+
+// create a rectangle object
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush.width = 5;
+
+function rendertoSVG()
+{
+  var svg = document.getElementById('renders');
+
+  var svgrender = canvas.toSVG();
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(svgrender, "image/svg+xml");
+
+  svg.innerHTML = doc.documentElement.outerHTML;
+
+  var path = doc.documentElement['children'][2]['childNodes'][1]['attributes'][2]
+
+  console.log(path);
+}
